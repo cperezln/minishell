cb1f55b65899ea8f30cceb578e36d8d40aeb800e
Cristian <c.perezc2018@gmail.com>
2020-12-05 13:07:15 +0100

    New funcionalities implemented; new errors discovered

--- minishell.c
+++ minishell.c
@@ -8,8 +8,7 @@
 int
 main(void) {
 	int status; //variable para que el padre espere
-	pid_t * listPid; //Lista de pids hijos
-	int * p[2]; //Puntero a pipe
+	int p[2]; //Puntero a pipe
 	char buf[1024];
 	tline * line;
 	int i,j, fdIn, fdOut, fdErr;
@@ -70,12 +69,80 @@ main(void) {
 			}
 		}
 		else{
-			for (i=0; i<line->ncommands; i++) {
+			char bufAux[1024];
+			pipe(p);
+			int pid1 = fork(); //primer hijo
+			if (pid1 == 0) {
+				if (boolIn) { //si hay red. de entrada
+					newInput = fopen(line-> redirect_input, "r"); //abrimos el fichero como escritura
+					fdIn = fileno(newInput);
+					dup2(fdIn, 0);
+					fclose(newInput);
+				}
+				close(p[0]);
+				dup2(p[1], 1); 
+				//fprintf(stderr, "Primer hijo\n");
+				execvp(line->commands[0].filename, line->commands[0].argv);
+				//fprintf(stderr, "Postex");
+				exit(1);
+			}
+			else{
+				wait(&status);
+				//read(p[0], buf, 1024);
+				//fprintf(stderr, "%s\n", buf);
+				int pAux[2];
+				pipe(pAux);
+				for (int i = 1; i < (line->ncommands - 1); i++) {	
+					//fprintf(stderr, "For dentro del padre");
+					/*read(p[0], buf, 1024);
+					write(pAux[1], buf, 1024);*/
+					dup2(p[0], pAux[1]);
+					int pid2 = fork();
+					if (pid2 == 0) {
+						//fprintf(stderr, "dentro del hijo2");
+						close(pAux[1]);
+						close(p[0]);
+						dup2(pAux[0], 0);
+						dup2(p[1], 1);
+						execvp(line->commands[i].filename, line->commands[i].argv);
+						exit(1);
+					}
+					else{
+						wait(&status);
+					}
+				}
+				//fprintf(stderr, "padre pero no for\n");
+				int pidFin = fork();
+				if (pidFin == 0) {
+					dup2(p[0], 0);
+					if (boolOut) { //si hay red. de salida
+						newOutput = fopen(line-> redirect_output, "w+"); //abrimos el fichero como escritura
+						fdOut= fileno(newOutput);
+						dup2(fdOut, 1);
+						fclose(newOutput);
+					}
+					/*if (boolErr) { //si hay red. de salida
+						newErr = fopen(line-> redirect_error, "w+"); //abrimos el fichero como escritura
+						fdErr= fileno(newErr);
+						dup2(fdErr, 2);
+						fclose(newErr);
+					}*/
+					fprintf(stderr, "Estamos dentro de hijo fin\n");
+					close(0);
+					execvp(line->commands[line->ncommands-1].filename, line->commands[line->ncommands-1].argv);
+					exit(1);
+				}
+				else{
+					fprintf(stderr, "A continuación del wait\n");
+					wait(NULL);
+				}
+			}
+			/*for (i=0; i<line->ncommands; i++) {
 				printf("orden %d (%s):\n", i, line->commands[i].filename);
 				for (j=0; j<line->commands[i].argc; j++) {
 					printf("  argumento %d: %s\n", j, line->commands[i].argv[j]);
 				}
-			}
+			}*/
 		}
 		printf("%s==> ",getcwd(directorioActual,-1));	
 	}
